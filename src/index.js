b128d26c47052ee5c70187a223cffd306e6eda74
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-05-25 12:09:57 -0400

    comienzo js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-/* Acá va tu código */
+var btnCS = document.getElementById('btnCS');
+var btnAbc = document.getElementById('btnAbc');
+
+btnCS.addEventListener('click', function(){
+    var ingresoCS = document.getElementById('ingresoCS').value; 
+    console.log(ingresoCS)
+});

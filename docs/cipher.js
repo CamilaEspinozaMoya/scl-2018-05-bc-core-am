098b3ec3978da6981c62795fdb7e5d0c9dc6b339
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-05-31 12:37:51 -0400

    carpeta docs agregada

---
+++ docs/cipher.js
@@ -0,0 +1,22 @@
+window.cipher = {
+  encode: (desplazamiento,ingresoCC) => {
+    /* Acá va tu código */
+  let result = "";
+  for (let i = 0; i< ingresoCC.length; i++ ) { //for recorre palabra
+    let posicion = ingresoCC.charCodeAt(i);  //pasa de letra a numero
+    let ascii = posicion - 65 + desplazamiento % 26 + 65; // aplica formula a la letra
+    result += String.fromCharCode(ascii); // pasa de numero a letra
+    }
+    return result;
+  },
+  decode: (desplazamiento,ingresoACifrar) => {
+      /* Acá va tu código */
+  let result2 = "";
+  for (let e = 0; e< ingresoACifrar.length; e++) {
+    let posicion2 = ingresoACifrar.charCodeAt(e);
+    let ascii2 = ((posicion2 + 65 - desplazamiento) % 26 + 65);
+    result2 += String.fromCharCode(ascii2);
+  }
+    return result2;
+  }
+}
---
+++ docs/index.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+
+const btnCC = document.getElementById('btnCC');
+const btnAbc = document.getElementById('btnAbc');
+const borrarCC = document.getElementById('borrarCC');
+const borrarAb = document.getElementById('borrarAb');
+const textoCC = document.getElementById('textoCC');
+const textoACifrar = document.getElementById('textoACifrar');
+
+// codificado de texto
+btnCC.addEventListener('click', () => {
+    let ingresoCC = document.getElementById('ingresoCC').value; 
+    console.log(ingresoCC);
+    let desplazamiento = document.getElementById('desplazamiento').value;
+    console.log(desplazamiento);
+    let enlaceCC = window.cipher.encode (desplazamiento,ingresoCC);
+    console.log(enlaceCC);
+    textoCC.innerHTML = enlaceCC;
+})
+
+// decodificado de texto
+btnAbc.addEventListener('click', () => {
+    let ingresoACifrar = document.getElementById('ingresoACifrar').value;
+    console.log(ingresoACifrar);
+    let desplazamiento = document.getElementById('desplazamiento').value;
+    console.log(desplazamiento);
+    let enlaceAb = window.cipher.decode (desplazamiento,ingresoACifrar);
+    textoACifrar.innerHTML = enlaceAb;
+});
+
+// borrado de texto codificado
+borrarCC.addEventListener('click', () => {
+    let ingresoCC = document.getElementById('ingresoCC').value = "";
+});
+
+// borrado de texto decodificado
+borrarAb.addEventListener('click', () => {
+    let ingresoACifrar = document.getElementById('ingresoACifrar').value = "";
+});
+
+}

eba81c13b70634f84c096c5cafe006442656d5b1
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-05-31 09:51:57 -0400

    agregando for y ascii, revisar anidado de css

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,11 @@
 window.cipher = {
-  encode: () => {
+  encode: (desplazamiento,ingresoCC) => {
     /* Acá va tu código */
-/*
-  const name = ingresoCC
-  let result = ""
-  for(let i = 0, i < name.length; i++) {
- result += name [i].toUpperCase();
- console.log(result);
+  let result = "";
+  for (let i = 0; i< ingresoCC.length; i++ ) {
+    let posicion = ingresoCC.charCodeAt(i);
+    let ascii = (posicion - 65 + desplazamiento) % 26 + 65;
   }
-  
-}
-*/
   },
   decode: () => {
       /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
+import { encode, decode } from "punycode";
+
 const btnCC = document.getElementById('btnCC');
 const btnAbc = document.getElementById('btnAbc');
-const borrarCC = document.getElementById('borrarCC') ;
-const borrarAb = document.getElementById('borrarAb') ;
-const textoCC = document.getElementById('textoCC') ;
-const textoACifrar = document.getElementById('textoACifrar') ;
+const borrarCC = document.getElementById('borrarCC');
+const borrarAb = document.getElementById('borrarAb');
+const textoCC = document.getElementById('textoCC');
+const textoACifrar = document.getElementById('textoACifrar');
 
 btnCC.addEventListener('click', () => {
     let ingresoCC = document.getElementById('ingresoCC').value; 
     console.log(ingresoCC);
-/*    textoCC.innerHTML = ingresoCC; */
+    textoCC.innerHTML = encode;
 })
 
 btnAbc.addEventListener('click', () => {
     let ingresoACifrar = document.getElementById('ingresoACifrar').value;
     console.log(ingresoACifrar);
-/*    textoACifrar.innerHTML = ingresoACifrar; */
+    textoACifrar.innerHTML = decode;
 });
 
 borrarCC.addEventListener('click', () => {

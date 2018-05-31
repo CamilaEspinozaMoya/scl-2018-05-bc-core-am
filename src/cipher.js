33a6fa8ee3a17389a0bffd02d9a87f8d922fec1c
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-05-31 14:14:29 -0400

    creo que ahora si corre el test

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,18 @@
 window.cipher = {
-  encode: (desplazamiento,ingresoCC) => {
+  encode: (ingresoCC,desplazamiento) => {
     /* Acá va tu código */
-  let result = [];
-  for (let i = 0; i< ingresoCC.length; i++ ) { //for recorre palabra
+  let result = "";
+  for (let i = 0; i < ingresoCC.length; i++ ) { //for recorre palabra
     //let posicion = ingresoCC.charCodeAt(i);  //pasa de letra a numero
     let ascii = ((ingresoCC.charCodeAt(i) - 65 + parseInt(desplazamiento)) % 26 + 65); // aplica formula a la letra
     result += String.fromCharCode(ascii); // pasa de numero a letra
     }
     return result;
   },
-  decode: (desplazamiento,ingresoACifrar) => {
+  decode: (ingresoACifrar,desplazamiento) => {
       /* Acá va tu código */
-  let result2 = [];
-  for (let i = 0; i< ingresoACifrar.length; i++) {
+  let result2 = "";
+  for (let i = 0; i < ingresoACifrar.length; i++) {
   //  let posicion2 = ingresoACifrar.charCodeAt(i);
     let ascii2 = ((ingresoACifrar.charCodeAt(i) + 65 - parseInt(desplazamiento)) % 26 + 65);
     result2 += String.fromCharCode(ascii2);
